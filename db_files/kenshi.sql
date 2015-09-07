b28c9b87b8431d4c8df890f9d957b3d09c814a3d
k0r3z <k0r3z@outlook.com>
2015-09-06 22:52:47 -0300

    0.0.3 SQL - kenshi.sql fixed and now working when imported to db

--- db_files/kenshi.sql
+++ db_files/kenshi.sql
@@ -1,28 +1,26 @@
-CREATE DATABASE IF NOT EXISTS kenshitool;
-
 CREATE TABLE IF NOT EXISTS `kenshi`(
-	`ID` int(6) unsigned NOT NULL auto_increment,
-	`name` varchar(20) NOT NULL,
-	`lastname` varchar(30) NOT NULL,
-	`resp` varchar(50) NOT NULL,
-	`birth` date NOT NULL,
-	`areacode` int(3) unsigned NOT NULL,
-	`phone` unsigned int(9) NOT NULL,
-	`photo` varchar(10),
-	`rank` varchar(8) NOT NULL,
-	`class` varchar(6) NOT NULL,
-	PRIMARY_KEY (`ID`),
+	`ID` int(6) NOT NULL auto_increment,
+	`name` varchar(20) DEFAULT NULL,
+	`lastname` varchar(30) DEFAULT NULL,
+	`resp` varchar(50) DEFAULT NULL,
+	`birth` date DEFAULT NULL,
+	`areacode` int(3) DEFAULT NULL,
+	`phone` int(9) DEFAULT NULL,
+	`photo` varchar(10) DEFAULT NULL,
+	`rank` varchar(8) DEFAULT NULL,
+	`class` varchar(6) DEFAULT NULL,
+	PRIMARY KEY (`ID`),
 	KEY `name` (`name`),
 	KEY `rank` (`rank`),
 	KEY `class` (`class`)
 ) ENGINE=MyISAM AUTO_INCREMENT=100000;
 
 CREATE TABLE IF NOT EXISTS financial(
-	`ID` int(6) unsigned NOT NULL,
-	`value` float(5) unsigned NOT NULL,
-	`year` int(4) unsigned NOT NULL,
-	`month` int(2) unsigned NOT NULL,
-	PRIMARY_KEY (`ID`),
+	`ID` int(6) NOT NULL,
+	`value` float(5) DEFAULT NULL,
+	`year` int(4) DEFAULT NULL,
+	`month` int(2) DEFAULT NULL,
+	PRIMARY KEY (`ID`),
 	KEY `year` (`year`),
 	KEY `month` (`month`)
 ) ENGINE=MyISAM;
