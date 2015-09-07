bfedd2e4f495d73add84510dfc56d1701bcf9599
k0r3z <k0r3z@outlook.com>
2015-09-06 22:34:18 -0300

    0.0.2 added financial table and translated some variables to english

--- db_files/kenshi.sql
+++ db_files/kenshi.sql
@@ -1,18 +1,28 @@
+CREATE DATABASE IF NOT EXISTS kenshitool;
+
 CREATE TABLE IF NOT EXISTS `kenshi`(
-	`ID` int(6) usigned NOT NULL auto_increment,
-	`nome` varchar(20) NOT NULL,
-	`sobrenome` varchar(30) NOT NULL,
-	`responsavel` varchar(50) NOT NULL,
-	`d_nascimento` date NOT NULL,
-	`ddd` int(3) unsigned NOT NULL,
-	`telefone` unsigned int(9) NOT NULL,
-	`foto` varchar(10),
+	`ID` int(6) unsigned NOT NULL auto_increment,
+	`name` varchar(20) NOT NULL,
+	`lastname` varchar(30) NOT NULL,
+	`resp` varchar(50) NOT NULL,
+	`birth` date NOT NULL,
+	`areacode` int(3) unsigned NOT NULL,
+	`phone` unsigned int(9) NOT NULL,
+	`photo` varchar(10),
 	`rank` varchar(8) NOT NULL,
-	`classe` varchar(6) NOT NULL,
+	`class` varchar(6) NOT NULL,
 	PRIMARY_KEY (`ID`),
-	KEY `nome` (`nome`),
+	KEY `name` (`name`),
 	KEY `rank` (`rank`),
-	KEY `classe` (`classe`)
+	KEY `class` (`class`)
 ) ENGINE=MyISAM AUTO_INCREMENT=100000;
 
-
+CREATE TABLE IF NOT EXISTS financial(
+	`ID` int(6) unsigned NOT NULL,
+	`value` float(5) unsigned NOT NULL,
+	`year` int(4) unsigned NOT NULL,
+	`month` int(2) unsigned NOT NULL,
+	PRIMARY_KEY (`ID`),
+	KEY `year` (`year`),
+	KEY `month` (`month`)
+) ENGINE=MyISAM;
