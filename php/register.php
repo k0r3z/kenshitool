ae6b03086a27de61256c99f36d2cbbc1f9ac8e4f
k0r3z <k0r3z@outlook.com>
2015-09-09 15:03:19 -0300

    0.0.7.3 another register.php test

--- php/register.php
+++ php/register.php
@@ -2,38 +2,23 @@
 //Requiring the connection file
 require 'connect.php';
 
-class Kenshi{
-	var $name;
-	var $l_name;
-	var $resp;
-	var $birth;
-	var $a_code;
-	var $phone;
-	var $photo;
-	var $rank;
-	var $clas;
-	function __construct() {
-	        $this->name = isset($_POST['name']) ? $_POST['name'] : null;
-		$this->name = isset($_POST['l_name']) ? $_POST['l_name'] : null;
-		$this->name = isset($_POST['resp']) ? $_POST['resp'] : null;
-		$this->name = isset($_POST['birth']) ? $_POST['birth'] : null;
-		$this->name = isset($_POST['a_code']) ? $_POST['a_code'] : null;
-		$this->name = isset($_POST['phone']) ? $_POST['phone'] : null;
-		//$this->name = isset($_POST['photo']) ? $_POST['photo'] : null;
-		$this->name = isset($_POST['rank']) ? $_POST['rank'] : null;
-		$this->name = isset($_POST['clas']) ? $_POST['clas'] : null;
-	
-	}
-}
-
 //Inserting data from html form
-$kenshi = new Kenshi();
-$sqlq = "INSERT INTO kenshi (name, lastname, resp, birth, areacode, phone, rank, class) VALUES ($kenshi.name, $kenshi.l_name, $kenshi.resp, $kenshi.birth, $kenshi.a_code, $kenshi.phone, $kenshi.rank, $kenshi.clas)";
+$name = $_POST['name'];
+$l_name = $_POST['l_name'];
+$resp = $_POST['resp'];
+$birth = $_POST['birth'];
+$a_code = $_POST['a_code'];
+$phone = $_POST['phone'];
+//$photo;
+$rank = $_POST['rank'];
+$clas = $_POST['clas'];
 
-if (mysqli_query($conn, $sqlq)) {
+$sqlq = "INSERT INTO kenshi (name, lastname, resp, birth, areacode, phone, rank, class) VALUES ($name, $l_name, $resp, $birth, $a_code, $phone, $rank, $clas)";
+
+if ($conn->query($sqlq) === TRUE) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sqlq . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sqlq . "<br>" . $conn->error;
 }
 
 //Closing connection from connect.php
